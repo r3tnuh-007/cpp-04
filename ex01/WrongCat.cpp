4d2e1232fa0b4b7f1101e1ace694cf1b4f9bf8cf
r3tnuh-007 <anterofranciso@gmail.com>
2026-09-12 20:59:05 +0100

    fixing the wrong... classes

---
+++ ex01/WrongCat.cpp
@@ -0,0 +1,31 @@
+#include "WrongCat.hpp"
+
+WrongCat::WrongCat()
+{
+    type = "WrongCat";
+    std::cout << "WrongCat constructor called" << std::endl;
+}
+
+WrongCat::WrongCat(const WrongCat& other) : WrongAnimal(other)
+{
+    std::cout << "WrongCat copy constructor called" << std::endl;
+    type = other.type;
+}
+
+WrongCat& WrongCat::operator=(const WrongCat& other)
+{
+    std::cout << "WrongCat assignment operator called" << std::endl;
+    if (this != &other)
+        type = other.type;
+    return *this;
+}
+
+WrongCat::~WrongCat()
+{
+    std::cout << "WrongCat destructor called" << std::endl;
+}
+
+void WrongCat::makeSound() const
+{
+    std::cout << "WrongCat meows (but not virtually)" << std::endl;
+}
